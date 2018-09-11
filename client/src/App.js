9ab97e70db5e1c5ae027c5ccf7a9a7cb836a26f9
eyalgromzin <ilyagromzy@gmail.com>
2018-09-11 17:51:45 +0300

    Revert "Revert "start 2""
    This reverts commit 946381b44c78d050b3d72031b6ba92d404b1c90a.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
-
 import { Provider } from 'react-redux';
 import store from './store';
+import Layout from './layout/layout'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -15,11 +15,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          <AppNavbar />
-          <Container>
-            <ItemModal />
-            <ShoppingList />
-          </Container>
+          <Layout /> 
         </div>
       </Provider>
     );
