11d8b35ad44d01a3444c3aaad0f50a38542601eb
eyalgromzin <ilyagromzy@gmail.com>
2018-09-14 18:02:53 +0300

    search db

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,80 +13,57 @@ router.get('/', (req, res) => {
     .then(items => res.json(items));
 });
 
+// @route   POST api/items
+// @desc    Create An Item
+// @access  Public
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    title: req.body.title,
+    content: req.body.content,
+    place: req.body.place,
+    minTime: req.body.minTime,
+    maxTime: req.body.maxTime,
+    minNumOfPeople: req.body.minNumOfPeople,
+    maxNumOfPeople: req.body.maxNumOfPeople
+  });
+});
+
+// @route   GET api/search/:place/:time/:numOfPeople/:more
+// @desc    search for anything
+// @access  Public
+router.get('/search/:place/:time/:numOfPeople', (req, res) => {   //
+  Item.find({
+    place: req.params.place,
+    $and:[
+      {minTime: {$lte: req.params.time}},
+      {maxTime: {$gte: req.params.time}}
+    ],
+    $and:[
+      {minNumOfPeople: {$lte: req.params.numOfPeople}},
+      {maxNumOfPeople: {$gte: req.params.numOfPeople}}
+    ]
+  })
+  .then(items => res.json(items));
+});
+
 // @route   GET api/search/:place/:time/:numOfPeople/:more
 // @desc    search for anything
 // @access  Public
 router.post('/search/', (req, res) => {
-  // Item.find(
-  //   {
-  //      gte : req.body.minTime,
-  //      lte : req.body.maxTime
-  // })
-
-var searchQuery = "Item.find({"
-
-if(req.body.place != ""){
-  searchQuery += "place: req.body.place,"
-}
-if(req.body.time != ""){
-  searchQuery += "$and:[" + 
-    "{minTime: {$lte: req.body.time}}," +
-    "{maxTime: {$gte: req.body.time}}" + 
-  "],"
-}
-if(req.body.numOfPeople != ""){
-  searchQuery += "$and:["
-    "{minNumOfPeople: {$lte: req.body.numOfPeople}},"
-    "{maxNumOfPeople: {$gte: req.body.numOfPeople}}" + 
-  "]"
-}
-if(req.body.more != ""){
-  searchQuery += "place: req.body.place,"
-}
-
-searchQuery += "})";
-
-Item.find({     //works
-  // eval("")
   
-  // place: req.body.place
-  // minTime: 30,
-  // $and:[
-  //   {minTime: {$lte: req.body.time}},
-  //   {maxTime: {$gte: req.body.time}}
-  // ],
-  // $and:[
-  //   {minNumOfPeople: {$lte: req.body.numOfPeople}},
-  //   {maxNumOfPeople: {$gte: req.body.numOfPeople}}
-  // ]
-})
-  //doesnt work
-  // Item.find({
-  //   time: {$gte: req.body.minTime},
-  //   time: {$lte: req.body.maxTime},
-  // })
-
-  // Item.find().and([{ time: {$gte: req.body.minTime}}, 
-  //                   {time: {$lte: req.body.maxTime}}]    //doesnt work
-
-  // Item.find(     //doesnt work
-  //   // {"time": {$gte: req.body.minTime, $lte: req.body.maxTime}} //fails
-  //   {$and: [
-  //     {"time": {$gte: req.body.minTime}},
-  //     {"time": {$lte: req.body.maxTime}}]
-  //   }
-  // )
-
-
-
-
-  // .where('place').equals(req.body.place)
-  // .where('time').gte(req.body.minTime).lte(req.body.maxTime) //doesnt work
+  Item.find({     //works
+    // place: req.body.place
+    // minTime: 30,
+    // $and:[
+    //   {minTime: {$lte: req.body.time}},
+    //   {maxTime: {$gte: req.body.time}}
+    // ],
+    $and:[
+      {minNumOfPeople: {$lte: req.body.time}},
+      {maxNumOfPeople: {$gte: req.body.time}}
+    ]
+  })
   .then(items => res.json(items));
-  
-  // .where('numOfPeople').gte(req.body.minNumOfPeople)
-  // .where('numOfPeople').lte(req.body.maxNumOfPeople)
-  // .then(items => res.json(items));
 });
 
 // @route   POST api/items
