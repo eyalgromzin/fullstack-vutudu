4e223aeefe385da01a2582c3316523727d807cd8
eyalgromzin <ilyagromzy@gmail.com>
2018-09-15 15:37:02 +0300

    finished basic add + search items

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
-import { SAVE_IDEAS } from 'reducers/types'
+import { SAVE_IDEAS,NO_ITEMS_FOUND } from 'reducers/types'
 import { connect } from 'react-redux';
 
 // Pass axios to the imported 'axios-debug' function.
@@ -30,10 +30,16 @@ export const searchItems = (place,time,numOfPeople) => dispatch => {
   axios
   .get(`/api/items/search/${place}/${time}/${numOfPeople}`)
   .then(res =>{
-    dispatch({
-      type: SAVE_IDEAS,
-      payload: res.data
-    })
+    if(res.data.length > 0){
+      dispatch({
+        type: SAVE_IDEAS,
+        payload: res.data
+      })
+    }else{
+      dispatch({
+        type: NO_ITEMS_FOUND
+      })
+    }
   }
   );
 };
