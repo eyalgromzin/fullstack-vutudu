c0675b767a66259c0ef67b401cf225969f83179f
eyalgromzin <eyalgromzin@gmail.com>
2020-04-15 17:01:04 +0300

    server.js update

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.use('/api/tagNames', tagNames);
 
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 // app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
 
