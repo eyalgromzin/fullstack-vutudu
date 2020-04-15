6eccea21b71cabbb0a19d40b95d9d1c0263200e4
eyalgromzin <eyalgromzin@gmail.com>
2020-04-15 16:01:44 +0300

    update number

--- client/src/components/searchLayout/searchLayout.js
+++ client/src/components/searchLayout/searchLayout.js
@@ -100,7 +100,7 @@ class searchLayout extends Component {
 							time={this.props.time}
 							more={this.props.more}
 						/>
-						4444
+						5555
 						{this.props.ideas.length == 0 && this.props.searched ? (
 							<NoResultsFound />
 						) : this.props.searched ? (
